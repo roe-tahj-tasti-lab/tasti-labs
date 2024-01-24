ec85dcd805e2fb1ae5212b7063e246011d7bc710
Tahj <tahjamie1@gmail.com>
2024-01-24 13:10:49 -0500

    removed app js

--- tasti-labs/src/fetch-category.js
+++ tasti-labs/src/fetch-category.js
@@ -1,20 +1,7 @@
-const getCatergory = async (idCategory) => {
-    try {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/${idCategory}.php`);
-        if(!response.ok) throw new Error("Sorry can't fetch food!")
-        const data = await response.json();
-        console.log(data);
 
-    }
-    catch(error){
-        console.error(error.message)
-    }
-}
-// When a user selects a category, you can then fetch the meals for that specific category.
-
-const getCatergoryDishes = async (idMeal) => {
+export const getCatergoryDishes = async (category) => {
     try {
-        const response = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${idMeal}`);
+        const response = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
         if(!response.ok) throw new Error("Sorry can't fetch food!")
         const data = await response.json();
         console.log(data);
