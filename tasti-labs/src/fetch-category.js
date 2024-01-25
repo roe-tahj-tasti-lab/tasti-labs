a3c299ca1b236140d7a4cbe981c1690d27269bdd
Tahj <tahjamie1@gmail.com>
2024-01-25 15:29:35 -0500

    reorganized

--- tasti-labs/src/fetch-category.js
+++ tasti-labs/src/fetch-category.js
@@ -4,6 +4,7 @@ export const getCategoryDishes = async (category) => {
         const response = await fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`);
         if(!response.ok) throw new Error("Sorry can't fetch food!")
         const data = await response.json();
+    console.log(data)
         return data 
     }
     catch(error){
